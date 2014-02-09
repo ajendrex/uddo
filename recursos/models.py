1574fffde8202464ccabd82cd40069efc3283c3e
ajendrex <hurbinas@gmail.com>
2014-02-09 07:50:17 -0300

    agregando campos de aprobacion de recursos, ignorando archivos python compilados

--- recursos/models.py
+++ recursos/models.py
@@ -31,6 +31,9 @@ class Recurso(models.Model):
   descripcion = models.TextField(blank=True)
   link = models.CharField(max_length = 300, blank=True)
   fecha_creacion = models.DateTimeField(auto_now_add = True)
+  aprobado_di = models.BooleanField(default=False)
+  aprobado_profesor = models.BooleanField(default=False)
+  aprobado_coordinador = models.BooleanField(default=False)
 
   def __str__(self):
     return self.nombre
