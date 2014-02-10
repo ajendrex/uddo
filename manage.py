0ffb1e439bdc089d5392ffd66cad42a0948cfef3
Hector Urbina <hurbinas@gmail.com>
2014-02-10 12:30:52 -0300

    forzando python 3.3

--- manage.py
+++ manage.py
@@ -1,6 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import os
 import sys
+
+cur_version = sys.version_info
+
+if cur_version.major < 3 or (cur_version.major == 3 and cur_version.minor < 3):
+  print("Este sitio no funcionará si la versión de python es menor a 3.3, por favor actualiza python.")
+  exit()
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uddo.settings")
